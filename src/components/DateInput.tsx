6e2f4030404d43a9269dbd11dbef7c5699ae1303
Rafael Chuman <rafael_chumansantana@outlook.com>
2023-05-25 13:50:09 -0300

    Fix data input in Firefox

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,37 +1,57 @@
-
 import React from "react";
 import { Component } from "react";
 
-import { format } from 'date-fns'
+import { format } from "date-fns";
 
-interface ComponentProps{
-  date:Date,
-  setDate:(newDateBegin: Date) => void
+interface ComponentProps {
+  date: Date;
+  setDate: (newDateBegin: Date) => void;
 }
 
-export class DateInput extends Component<ComponentProps>{
-
+export class DateInput extends Component<ComponentProps> {
   render() {
-
-    const  handleChangeDate = (dateInput: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChangeDate = (
+      dateInput: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      let validated = true;
+      let dateConverted: number[] =[];
 
       //This Replace is a crazy think of JavaScript that don't convert correctly dates with the caracter "-"
+      let DateValues = dateInput.target.value.split("-");
+      if (DateValues && DateValues.length == 3) {
+        dateConverted = DateValues.map((item) => {
+          if (item) return parseInt(item);
+          validated = false;
+          return 0;
+        });
+      } else {
+        validated = false;
+      }
 
-      const newDate = new Date(dateInput.target.value.replace("-", "/"));
-      this.props.setDate(newDate);
-    }
-   
+      if (validated) {
+        let newDate = new Date(dateConverted[0], dateConverted[1]-1, dateConverted[2]);
+
+        this.props.setDate(newDate);
+      }
+    };
+
     const date = new Date();
 
-    const today =  format(date, 'yyyy-MM-dd')
+    const today = format(date, "yyyy-MM-dd");
 
-    date.setFullYear(date.getFullYear()-1);
+    date.setFullYear(date.getFullYear() - 1);
 
-    const lastYear = format(date, 'yyyy-MM-dd')
-
+    const lastYear = format(date, "yyyy-MM-dd");
 
     return (
-      <input type="date" min={lastYear} max={today} defaultValue={format(this.props.date, 'yyyy-MM-dd')} onChange={ e => handleChangeDate(e)} required></input>
+      <input
+        type="date"
+        min={lastYear}
+        max={today}
+        defaultValue={format(this.props.date, "yyyy-MM-dd")}
+        onChange={(e) => handleChangeDate(e)}
+        required
+      ></input>
     );
   }
 }
